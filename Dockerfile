757727ac8c93ad92689f418cfb479cae927230c8
John Maguire <john.maguire@gmail.com>
2017-12-19 22:16:49 +0000

    Clementine-specific adjustments

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM voidlinux/voidlinux
 MAINTAINER Andrea Brancaleoni "miwaxe@gmail.com"
 
 RUN xbps-install -Syyu &&\
+	xbps-install -Syy git curl &&\
 	mkdir -p /run/runit/runsvdir/current &&\
 	xbps-install -Syu socklog-void &&\
 	ln -s /etc/sv/socklog-unix /var/service/ &&\
@@ -14,7 +15,10 @@ RUN xbps-install -Syyu &&\
 RUN xbps-install -Syyu qemu && mkdir -p /etc/sv/qemu /usr/lib/qemu/ && ln -s /etc/sv/qemu /var/service
 ADD enoch_rev2839_boot /usr/lib/qemu/
 ADD boot.sh /etc/sv/qemu/run
+ADD OVMF_CODE-pure-efi.fd /usr/lib/qemu/
+ADD OVMF_VARS-pure-efi-1024x768.fd /usr/lib/qemu/
+ADD Clover.qcow2 /usr/lib/qemu/
 
-EXPOSE 2222 5800 5900
+EXPOSE 2222 5800 5900 5001
 
 CMD runsvdir -P /var/service
--- boot.sh
+++ boot.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
-# qemu-img create -f qcow2 mac_hdd.img 64G
-# echo 1 > /sys/module/kvm/parameters/ignore_msrs
+# See https://www.mail-archive.com/qemu-devel@nongnu.org/msg471657.html thread.
 #
-# Type the following after boot,
-# -v "KernelBooter_kexts"="Yes" "CsrActiveConfig"="103"
+# The "pc-q35-2.4" machine type was changed to "pc-q35-2.9" on 06-August-2017.
 #
-# printf 'DE:AD:BE:EF:%02X:%02X\n' $((RANDOM%256)) $((RANDOM%256))
-#
-# no_floppy = 1 is required for OS X guests!
-#
-# Commit 473a49460db0a90bfda046b8f3662b49f94098eb (qemu) makes "no_floppy = 0"
-# for pc-q35-2.3 hardware, and OS X doesn't like this (it hangs at "Waiting for
-# DSMOS" message). Hence, we switch to pc-q35-2.4 hardware.
-#
-# Network device "-device e1000-82545em" can be replaced with "-device vmxnet3"
-# for possibly better performance.
+# The "media=cdrom" part is needed to make Clover recognize the bootable ISO
+# image.
+
+##################################################################################
+# NOTE: Comment out the "MY_OPTIONS" line in case you are having booting problems!
+##################################################################################
 BACKING_DIR=/backing
 SNAPSHOT_DIR=/snapshot
 mkdir -p $BACKING_DIR $SNAPSHOT_DIR
 [ ! -f $SNAPSHOT_DIR/mac_hdd.img ] && qemu-img create -f qcow2 -b $BACKING_DIR/mac_hdd-backing.img $SNAPSHOT_DIR/mac_hdd.img
 
-if [ -c /dev/kvm ]; then
-	KVM_ARGS='-enable-kvm'
-fi
 
-exec qemu-system-x86_64 $KVM_ARGS -m 8192 -cpu core2duo,kvm=off \
-	  -machine pc-q35-2.4 \
+MY_OPTIONS="+aes,+xsave,+avx,+xsaveopt,avx2,+smep"
+
+exec qemu-system-x86_64 -enable-kvm -m 3072 -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS\
+    -vnc 0.0.0.0:0 \
+    -redir tcp:2222::22 \
+    -redir tcp:5001::5001 \
+	  -machine pc-q35-2.9 \
 	  -smp 4,cores=2 \
 	  -usb -device usb-kbd -device usb-tablet \
 	  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
-	  -kernel /usr/lib/qemu/enoch_rev2839_boot \
+	  -drive if=pflash,format=raw,readonly,file=/usr/lib/qemu/OVMF_CODE-pure-efi.fd \
+	  -drive if=pflash,format=raw,file=/usr/lib/qemu/OVMF_VARS-pure-efi-1024x768.fd \
 	  -smbios type=2 \
-	  -device ide-drive,bus=ide.2,drive=MacHDD \
-	  -drive id=MacHDD,if=none,file=$SNAPSHOT_DIR/mac_hdd.img \
-      	  -netdev user,id=usr0 -device e1000-82545em,netdev=usr0,id=vnet0 \
 	  -device ich9-intel-hda -device hda-duplex \
-	  -display none -redir tcp:2222::22 -vnc 0.0.0.0:0
+	  -device ide-drive,bus=ide.2,drive=Clover \
+	  -drive id=Clover,if=none,snapshot=on,format=qcow2,file=/usr/lib/qemu/Clover.qcow2 \
+	  -device ide-drive,bus=ide.1,drive=MacHDD \
+	  -drive id=MacHDD,if=none,file=$SNAPSHOT_DIR/mac_hdd.img,format=qcow2 \
+	  -netdev user,id=net0 -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+	  -monitor stdio
